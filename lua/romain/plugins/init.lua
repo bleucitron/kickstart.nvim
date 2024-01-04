3ef18693dd613ea230dfebd4c9f891c3580c4445
Romain Crestey <rcrestey@gmail.com>
2024-01-04 16:54:12 +0100

    add harpoon, null-ls and prettier on save

--- lua/romain/plugins/init.lua
+++ lua/romain/plugins/init.lua
@@ -1,3 +1,51 @@
 return {
-	'ThePrimeagen/vim-be-good'
+	'ThePrimeagen/vim-be-good',
+	{
+		"ThePrimeagen/harpoon",
+		opts = function()
+			local harpoon = require('harpoon')
+
+			vim.keymap.set("n", "<leader>=", function() harpoon:list():append() end)
+			vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+			vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+			vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+			vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+			vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+			vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+		end,
+		branch = "harpoon2"
+	},
+	{
+		'jose-elias-alvarez/null-ls.nvim',
+		opts = function ()
+			local null_ls = require('null-ls')
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			local formatting = null_ls.builtins.formatting
+
+			return {
+				sources = {
+					formatting.prettierd
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end
+			}
+		end
+	}
 }
