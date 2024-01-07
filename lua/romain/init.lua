d9ba6f8830cdb2471ad318c82d64f0ca695a6a1c
Romain Crestey <rcrestey@gmail.com>
2024-01-07 17:22:23 +0100

    add more keymaps

--- lua/romain/init.lua
+++ lua/romain/init.lua
@@ -19,8 +19,23 @@ vim.o.colorcolumn = "80"
 -- Remaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- moving selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- edit every occurence of the word you at
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
+-- in any visual mode, keep the copy registry consistent
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy into system's clipboard register
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+-- center screen on page moves
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
