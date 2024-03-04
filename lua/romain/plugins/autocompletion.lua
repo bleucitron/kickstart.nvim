a733744b47c9a44062def31f17069fda3bee489a
Romain Crestey <rcrestey@gmail.com>
2024-03-04 23:40:05 +0100

    comment out weird mappings

--- lua/romain/plugins/autocompletion.lua
+++ lua/romain/plugins/autocompletion.lua
@@ -48,15 +48,17 @@ return { -- Autocompletion
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
-        -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        -- NOTE: [Romain] i don't think i like these mappings
+        --
+        -- -- Select the [n]ext item
+        -- ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- -- Select the [p]revious item
+        -- ['<C-p>'] = cmp.mapping.select_prev_item(),
+        --
+        -- -- Accept ([y]es) the completion.
+        -- --  This will auto-import if your LSP supports it.
+        -- --  This will expand snippets if the LSP sent a snippet.
+        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
         -- Manually trigger a completion from nvim-cmp.
         --  Generally you don't need this, because nvim-cmp will display
