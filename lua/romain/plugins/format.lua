4ed9e600764177d97765cde11ae4dac71ff80590
Romain Crestey <rcrestey@gmail.com>
2024-03-04 10:13:39 +0100

    structure plugins

---
+++ lua/romain/plugins/format.lua
@@ -0,0 +1,54 @@
+local function organize_imports()
+  local params = {
+    command = '_typescript.organizeImports',
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = '',
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local organize_imports_types = { 'js', 'ts', 'svelte' }
+      if vim.tbl_contains(organize_imports_types, vim.bo[bufnr].filetype) then
+        organize_imports()
+      end
+
+      return {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use a sub-list to tell conform to run *until* a formatter
+      -- is found.
+      javascript = { 'prettierd' },
+      typescript = { 'prettierd' },
+      svelte = { 'prettierd' },
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      json = { 'prettierd' },
+      yaml = { 'prettierd' },
+      markdown = { 'prettierd' },
+      graphql = { 'prettierd' },
+    },
+  },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = 'Format buffer',
+    },
+  },
+}
--- lua/romain/plugins/init.lua
+++ lua/romain/plugins/init.lua
@@ -39,41 +39,6 @@ return {
     },
   },
   {
-    'stevearc/conform.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local conform = require 'conform'
-
-      conform.setup {
-        formatters_by_ft = {
-          javascript = { 'prettierd' },
-          typescript = { 'prettierd' },
-          svelte = { 'prettierd' },
-          css = { 'prettierd' },
-          html = { 'prettierd' },
-          json = { 'prettierd' },
-          yaml = { 'prettierd' },
-          markdown = { 'prettierd' },
-          graphql = { 'prettierd' },
-          lua = { 'stylua' },
-        },
-        format_on_save = {
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 500,
-        },
-      }
-
-      vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
-        conform.format {
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 500,
-        }
-      end, { desc = 'Format file or range (in visual mode)' })
-    end,
-  },
-  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
     dependencies = {
