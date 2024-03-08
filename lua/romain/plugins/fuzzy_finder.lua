a976f5e19afafa8fda871bfe0ce0b437d0481bd6
Romain Crestey <rcrestey@gmail.com>
2024-03-08 15:09:19 +0100

    telescope better config

--- lua/romain/plugins/fuzzy_finder.lua
+++ lua/romain/plugins/fuzzy_finder.lua
@@ -46,7 +46,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
-    require('telescope').setup {
+
+    local telescope = require 'telescope'
+    local telescopeConfig = require 'telescope.config'
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+    -- I want to search in hidden/dot files.
+    table.insert(vimgrep_arguments, '--hidden')
+    -- I don't want to search in the `.git` directory.
+    table.insert(vimgrep_arguments, '--glob')
+    table.insert(vimgrep_arguments, '!**/.git/*')
+
+    telescope.setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
@@ -59,6 +70,16 @@ return { -- Fuzzy Finder (files, lsp, etc)
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
+        },
+      },
+      defaults = {
+        -- `hidden = true` is not supported in text grep commands.
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
         },
       },
     }
