5b1110c2ebaf18d5112850fca44173e9ed3ab0c1
Romain Crestey <rcrestey@gmail.com>
2024-04-04 16:30:44 +0200

    use backspace as leader

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = vim.api.nvim_replace_termcodes('<BS>', false, false, true) -- Backspace as leader key
+vim.g.maplocalleader = vim.api.nvim_replace_termcodes('<BS>', false, false, true) -- Backspace as leader key
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
