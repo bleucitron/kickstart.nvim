a7377bdbd6a42a9911331d6fa5dda749bc95b898
Romain Crestey <rcrestey@gmail.com>
2024-09-02 17:24:42 +0200

    adds delete buffer action

--- lua/romain/plugins/fuzzy_finder.lua
+++ lua/romain/plugins/fuzzy_finder.lua
@@ -49,6 +49,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     local telescope = require 'telescope'
     local telescopeConfig = require 'telescope.config'
+    local actions = require 'telescope.actions'
     local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
     -- I want to search in hidden/dot files.
@@ -81,6 +82,13 @@ return { -- Fuzzy Finder (files, lsp, etc)
         find_files = {
           -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
           find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+        },
+        buffers = {
+          mappings = {
+            i = {
+              ['<c-d>'] = actions.delete_buffer + actions.move_to_top,
+            },
+          },
         },
       },
     }
